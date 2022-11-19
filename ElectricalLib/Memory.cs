bd0352c48fa9facdc794bb8e243b826f69dbd166
Андрей Боев <a.boev.it@gmail.com>
2022-11-19 21:32:07 +0300

    19.11.2022

---
+++ ElectricalLib/Memory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricalLib
+{
+    public class Memory : Element
+    {
+        private bool[] inputs;
+        private bool outValue;
+        private bool notOutValue;
+
+        public Memory()
+        {
+            inputs = new bool[3];
+            outValue = false;
+            notOutValue = false;
+        }
+
+        public Memory(Memory memory) : base("Memory", 3, 1)
+        {
+            memory.outValue = outValue;
+            memory.inputs.CopyTo(inputs, 0);
+            memory.notOutValue = notOutValue;
+        }
+
+        public void SetInputs(bool[] inputs)
+        {
+            this.inputs.CopyTo(inputs, 0);
+        }
+
+        public bool GetInput(int inputIndex)
+        {
+            return inputs[inputIndex];
+        }
+
+        public bool GetValue()
+        {
+            // TODO: Переделать
+            bool result = inputs[0];
+            for (int i = 1; i < base.InCount; i++)
+                result = (result && inputs[i]);
+            return !result;
+        }
+
+        public bool GetOut()
+        {
+            return this.outValue;
+        }
+
+        public bool GetNotOut()
+        {
+            return this.notOutValue;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+                return false;
+            Memory other = obj as Memory;
+            return other != null && outValue == other.outValue && notOutValue == other.notOutValue && inputs.Equals(other.inputs);
+        }
+
+        public static bool operator ==(Memory m1, Memory m2)
+        {
+            return m1.Equals(m2);
+        }
+
+        public static bool operator !=(Memory m1, Memory m2)
+        {
+            return !(m1 == m2);
+        }
+    }
+}
---
+++ ElectricalLib/Register.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricalLib
+{
+    public class Register
+    {
+        private Memory[] data;
+        private bool[,] inputs;
+        private bool set;
+        private bool reset;
+        public Register()
+        {
+            data = new Memory[12];
+            inputs = new bool[12, 3];
+        }
+    }
+}
